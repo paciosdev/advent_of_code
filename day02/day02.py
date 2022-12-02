bd0ab24db872f2548352c43f605e7234bdf70d1a
Francesco Paciello <kekkopacio@gmail.com>
2022-12-02 11:57:55 +0100

    day02 completed

--- day02/day02.py
+++ day02/day02.py
@@ -51,7 +51,61 @@ def calculateScore(opponentMove, myMove):
 
     return score
 
+    
+
+
 for game in games:
     sum += calculateScore(game.opponentMove, game.myMove)
+
+print(sum)
+
+
+sum = 0
+
+def calculateScore2(opponentMove, myMove):
+
+    score = 0
+
+    #Avversario ha scelto sasso
+    if opponentMove == "A":
+        if myMove == "X":
+            #perdo con forbice 
+            score += 3
+        elif myMove == "Y":
+            #pareggio con sasso 
+            score += 4
+        elif myMove == "Z":
+            #vinco con carta 
+            score += 8
+
+    #Avversario ha scelto carta
+    elif opponentMove == "B":
+        if myMove == "X": 
+            #perdo con sasso 
+            score += 1
+        elif myMove == "Y":
+            #pareggio con carta 
+            score += 5
+        elif myMove == "Z":
+            #vinco con forbice
+            score += 9
+
+    #Avversario ha scelto forbice
+    elif opponentMove == "C":
+        #perdo con carta 
+        if myMove == "X":
+            score += 2
+        #pareggio con forbice
+        elif myMove == "Y":
+            score += 6
+        #vinco con sasso
+        elif myMove == "Z":
+            score += 7
+
+    return score
+
+
+for game in games:
+    sum += calculateScore2(game.opponentMove, game.myMove)
 
 print(sum)
