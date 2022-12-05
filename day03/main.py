8be45e951ff27fb3364bc60ab1b66a2e6628d546
Francesco Paciello <kekkopacio@gmail.com>
2022-12-05 08:04:58 +0100

    day03 completed

---
+++ day03/main.py
@@ -0,0 +1,98 @@
+f = open("ruckstacks.txt", "r")
+
+priorities = {
+    "a" : 1,
+    "b" : 2,
+    "c" : 3,
+    "d" : 4,
+    "e" : 5,
+    "f" : 6,
+    "g" : 7,
+    "h" : 8,
+    "i" : 9,
+    "j" : 10,
+    "k" : 11,
+    "l" : 12,
+    "m" : 13,
+    "n" : 14,
+    "o" : 15,
+    "p" : 16,
+    "q" : 17,
+    "r" : 18,
+    "s" : 19,
+    "t" : 20,
+    "u" : 21,
+    "v" : 22,
+    "w" : 23,
+    "x" : 24,
+    "y" : 25,
+    "z" : 26,
+    "A" : 27,
+    "B" : 28,
+    "C" : 29,
+    "D" : 30,
+    "E" : 31,
+    "F" : 32,
+    "G" : 33,
+    "H" : 34,
+    "I" : 35,
+    "J" : 36,
+    "K" : 37,
+    "L" : 38,
+    "M" : 39,
+    "N" : 40,
+    "O" : 41,
+    "P" : 42,
+    "Q" : 43,
+    "R" : 44,
+    "S" : 45,
+    "T" : 46,
+    "U" : 47,
+    "V" : 48,
+    "W" : 49,
+    "X" : 50,
+    "Y" : 51,
+    "Z" : 52,
+}
+
+def firstPart():
+    sum = 0
+
+    for line in f.readlines():
+        centerIndex = int(len(line) / 2)
+        left, right = line[:centerIndex], line[centerIndex:]
+
+        common_char = ''.join(
+        set(left).intersection(right)
+        )
+        sum += priorities[common_char]
+
+    print(sum)
+
+
+
+
+def secondPart():
+
+    sum = 0
+
+    group = []
+    count = 0
+    for line in f.readlines():
+
+        group.append(line)
+        
+        count += 1
+
+        if count == 3:
+            common_char = ''.join(
+        set(group[0]).intersection(group[1]).intersection(group[2])
+        )
+            group = []
+            count = 0
+            print(common_char)
+            sum += priorities[common_char.replace("\n", "")]
+    print(sum)
+
+
+secondPart()
